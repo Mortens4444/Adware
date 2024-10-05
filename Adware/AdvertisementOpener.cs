10aedd84ebe8c49006a2d6ad636e705ae9549afa
Mortens <mortens.4444@gmail.com>
2024-10-05 19:56:22 +0200

    Initial commit

---
+++ Adware/AdvertisementOpener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Adware
+{
+    public class AdvertisementOpener
+    {
+        readonly Dictionary<string, string> pageTitleParts;
+
+        public AdvertisementOpener(Dictionary<string, string> pageTitleParts)
+        {
+            this.pageTitleParts = pageTitleParts;
+        }
+
+        public bool OpenUnwantedAdvertisements()
+        {
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                foreach (var pageTitlePart in pageTitleParts)
+                {
+                    var lowerCaseTitle = process.MainWindowTitle.ToLower();
+                    if (lowerCaseTitle.Contains(pageTitlePart.Key))
+                    {
+                        Process.Start(pageTitlePart.Value);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Adware/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Adware
+{
+    internal static class Program
+    {
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr WindowHandle, NCmdShow nCmdShow);
+
+        private static readonly Dictionary<string, string> PageTitleParts = new Dictionary<string, string>
+        {
+            { "garden", "https://play.google.com/store/apps/details?id=com.mortens.kertmester" },
+            { "developer", "https://github.com/Mortens4444" }
+        };
+
+        static void Main()
+        {
+            ShowWindow(Process.GetCurrentProcess().MainWindowHandle, NCmdShow.SW_HIDE);
+
+            var advertisementOpener = new AdvertisementOpener(PageTitleParts);
+            while (true)
+            {
+                var advertisementOpened = advertisementOpener.OpenUnwantedAdvertisements();
+                Thread.Sleep(advertisementOpened ? (int)TimeSpan.FromHours(1).TotalMilliseconds : 1000);
+            }
+        }
+    }
+}
